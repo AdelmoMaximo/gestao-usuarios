27adf2ccc78d12b3e073ca6fff85079cb8b9c59f
adelmomaximo <adelmomaximo661@gmail.com>
2025-02-11 20:31:13 -0400

    chore: atualiza .gitignore e remove arquivos desnecessários

--- backend/dist/usuario/usuario.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateUsuarioDto {
-    nome: string;
-    email: string;
-    matricula: string;
-    senha: string;
-}
-export declare class UpdateUsuarioDto extends CreateUsuarioDto {
-}
